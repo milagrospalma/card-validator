683f2c5810c0ffebc5b7a6fcf3f2089d5c54beed
milagrospalma <mnpalma.2@gmail.com>
2018-01-29 12:56:24 -0500

    Añade estructura de carpetas y funcionalidad

--- src/app.js
+++ src/app.js
@@ -1,32 +1,32 @@
-window.addEventListener('load', function () {
-  var cn = document.getElementById('cn');
-  var pay = document.getElementById('pay');
-  var formCn = document.getElementById('fg-cn');
+window.addEventListener('load', () => {
+  let inputCn = document.getElementById('cn');
+  var inputExp = document.getElementById('exp');
+  let formCn = document.getElementById('fg-cn');
 
-  cn.addEventListener('input', function () {
+  inputCn.addEventListener('input', () => {
     console.log(cn.value);
     isValidCard(cn.value);
   });
 
-  cvv.addEventListener('input', function () {
+  cvv.addEventListener('input', () => {
     console.log(cvv.value);
     isValidCvv(cvv.value);
   });
 
   // Función para validar tarjeta
-  var isValidCard = function (string) {
-    var array = [], sum = 0;
-    for (var i = 0; i < string.length; i++) {
+  let isValidCard = (string) => {
+    let array = [], sum = 0;
+    for (let i = 0; i < string.length; i++) {
       array.push(parseInt(string[i]));
     }
-    for (var i = 0; i < array.length; i++) {
+    for (let i = 0; i < array.length; i++) {
       if (i % 2 === 0) {
-        var value = array[i] * 2;
+        let value = array[i] * 2;
         // Reemplaza los nuevos valores en la misma posición
         array.splice(i, 1, value);
         if (value >= 10) {
-          var strValue = value.toString();
-          var newNumber = parseInt(strValue[0]) + parseInt(strValue[1]);
+          let strValue = value.toString();
+          let newNumber = parseInt(strValue[0]) + parseInt(strValue[1]);
           // Reemplaza los nuevos valores en la misma posición
           array.splice(i, 1, newNumber);
         }
@@ -46,7 +46,7 @@ window.addEventListener('load', function () {
   };
 
   // Función para validar cvv
-  var isValidCvv = function (cvv) {
+  let isValidCvv = (cvv) => {
     if (cvv.length === 3) {
       console.log('longitud válida');
     };
