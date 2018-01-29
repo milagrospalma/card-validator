76b5c20f6e9066b02b4c1440928a7fe31a36ecce
milagrospalma <mnpalma.2@gmail.com>
2018-01-29 08:21:16 -0500

    Añade validación de tarjeta

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
-# Valida datos de tarjetas de crédito
+# Validador de datos de Tarjetas de Crédito
 
-* **Track:** _Common Core_
-* **Curso:** _JS Deep Dive: Crea tu propia librería usando JavaScript_
-* **Unidad:** _Producto final_
+## Objetivo
+Implementar un validador de datos de tarjeta de crédito. La librería debería validar el número de tarjeta de crédito (usando algoritmo de Luhn), fecha de vencimiento, codigo de verificación (cvv) y nombre completo que aparece en la tarjeta.
 
-***
-
+## Descripción
 El plugin debe recibir una referencia a un elemento del DOM que contenga
 `<input>`s con los siguientes nombres (atributo `name`):
 
@@ -54,3 +52,9 @@ form.addEventListener('submit', (e) => {
 A la hora de hacer las validaciones, la librería debería de añadir la clase
 `.error` a los `<input>`s que no pasen la validación, o la clase `.success`
 en caso de que sí pase.
+
+## Vista demo
+![Demo](src/assets/docs/demo.png)
+
+## Herramientas
+`HTML5` `CSS3` `JavaScript` `jQuery` `Bootstrap`
---
+++ src/js/index.js
@@ -0,0 +1,54 @@
+window.addEventListener('load', function () {
+  var cn = document.getElementById('cn');
+  var pay = document.getElementById('pay');
+  var formCn = document.getElementById('fg-cn');
+
+  cn.addEventListener('input', function () {
+    console.log(cn.value);
+    isValidCard(cn.value);
+  });
+
+  cvv.addEventListener('input', function () {
+    console.log(cvv.value);
+    isValidCvv(cvv.value);
+  });
+
+  // Función para validar tarjeta
+  var isValidCard = function (string) {
+    var array = [], sum = 0;
+    for (var i = 0; i < string.length; i++) {
+      array.push(parseInt(string[i]));
+    }
+    for (var i = 0; i < array.length; i++) {
+      if (i % 2 === 0) {
+        var value = array[i] * 2;
+        // Reemplaza los nuevos valores en la misma posición
+        array.splice(i, 1, value);
+        if (value >= 10) {
+          var strValue = value.toString();
+          var newNumber = parseInt(strValue[0]) + parseInt(strValue[1]);
+          // Reemplaza los nuevos valores en la misma posición
+          array.splice(i, 1, newNumber);
+        }
+      }
+      // Suma los elementos de la posición i
+      sum += array[i];
+    }
+    if (sum % 10 === 0 && string.trim() !== '' && string.length === 16) {
+      formCn.removeClass = 'has-error';
+      formCn.className = 'has-success';
+      console.log('válida');
+    } else {
+      formCn.removeName = 'has-success';
+      formCn.className = 'has-error';
+      console.log('inválida');
+    }
+  };
+
+  // Función para validar cvv
+  var isValidCvv = function (cvv) {
+    if (cvv.length === 3) {
+      console.log('longitud válida');
+    };
+  };
+});
